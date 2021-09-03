2935f49e6c0b18e0b79c97fa0128cc1a85fdc246
Crowdbotics <team@crowdbotics.com>
2021-09-03 18:03:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "roody_itech_30297Identifier",
+  url: "https://roody_itech_30297Identifier.botics.co",
+  api: "https://roody_itech_30297Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
